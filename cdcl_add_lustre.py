9d85f34a1a8eee79afda20d74048dc63a960a004
Julian M. Kunkel <juliankunkel@googlemail.com>
2021-05-03 21:36:46 +0200

    Add number of nodes automatically to schema.

--- cdcl_add_lustre.py
+++ cdcl_add_lustre.py
@@ -11,21 +11,76 @@ import traceback
 import subprocess
 from optparse import OptionParser
 
-#from cdcl_info_editor import edit_infos, execute_download
+from cdcl_info_editor import edit_infos, execute_download
+
+def parseOBDS(data, state):
+  options.lustreFS = state["oss_fs"]
+  max_ds = 0  
+  for line in data:
+    if line == "\n":
+      info("OSS.count", max_ds)
+      return
+    if line == "OBDS:\n":
+      info("OSS.count", max_ds)
+      max_ds = 0
+      state["oss_fs"] = state["oss_fs"] + 1      
+      options.lustreFS = state["oss_fs"]
+      continue
+    m = re.match("([0-9]+): (.*) ", line)
+    if m:
+      max_ds = int(m.group(1))
+    else:
+      print("Parsing error OBDS: " + line)
+
+def parseMDTS(data, state):
+  options.lustreFS = state["mds_fs"]
+  max_ds = 0
+  for line in data:
+    if line == "\n":
+      info("MDS.count", max_ds)
+      state["mds_fs"] = state["mds_fs"] + 1
+      return
+    if line == "MDTS:\n":    
+      info("MDS.count", max_ds)
+      max_ds = 0
+      state["mds_fs"] = state["mds_fs"] + 1
+      options.lustreFS = state["mds_fs"]
+      continue
+    m = re.match("([0-9]+): (.*) ", line)
+    if m:
+      max_ds = m.group(1)
+    else:
+      print("Parsing error MDTS: " + line)
 
 def parse(data):
   # TODO
+  state = { "mds_fs" : 0, "oss_fs" : 0}
   for line in data:
-    print(line)
+    if line == "OBDS:\n":
+      parseOBDS(data, state)
+    if line == "MDTS:\n":
+      parseMDTS(data, state)
+    
+def infoL(key, val, unit = ""):
+  global cmd, options
+  val = str(val).strip()
+  unit = unit.strip()
+  if val == None or val == "":
+    return
+  cmd.append(("Site.StorageSystem[%s].Lustre." % (options.lustreFS) + key + "=" + val + " " + unit).strip())
+
+def info(key, val, unit = ""):
+  infoL(key, val)
 
 parser = OptionParser()
 parser.add_option("-j", "--json", dest="json",
+                  default="site.json",
                   help="Update this JSON file with the new data", metavar="FILE")
 parser.add_option("-f", "--file", dest="filename",
                   help="read data from FILE", metavar="FILE")
-parser.add_option("-l", "--lustreFS",
-                  dest="lustreFS", default="0",
-                  help="The Lustre FS in the schema to fill", metavar="NUMBER")
+#parser.add_option("-l", "--lustreFS",
+#                  dest="lustreFS", default="0",
+#                  help="The Lustre FS in the schema to fill", metavar="NUMBER")
 
 (options, args) = parser.parse_args()
 options.loadFromFile = False
@@ -68,8 +123,10 @@ else:
   if options.filename:
     print("Saving to file %s" % options.filename)
     file = open(options.filename, 'w')
-    file.write(data)
+    file.write(data + "\n")
     file.close()
   data = data.split("\n")
 
+cmd = []
 parse(data)
+edit_infos(options.json, cmd)
