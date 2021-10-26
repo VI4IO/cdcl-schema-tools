bbe05ecf82184e0e5a30a56c612543d2abb09027
Julian M. Kunkel <juliankunkel@googlemail.com>
2021-10-26 15:54:12 +0200

    Extended run data.

--- cdcl_add_io500_results.py
+++ cdcl_add_io500_results.py
@@ -19,11 +19,16 @@ config.read(result + '/result.txt')
 
 def add_res(key, ini_section, unit, ini_key = "score"):
   global config, cmd
-
+  if not ini_section in config:
+    print("Cannot find " + ini_section + " section")
+    return
+  if not ini_key in config[ini_section]:
+    print("Cannot find key " + ini_key + " in " + ini_section + " section")
+    return
   cmd.append(key + "=" + config[ini_section][ini_key] + " " + unit)
 
 cmd = []
-for op in ["easy write", "easy read", "hard write", "hard read"] :
+for op in ["easy write", "easy read", "hard write", "hard read", "rnd1MB write", "rnd1MB read", "rnd4K write", "rnd4K read"] :
   t = op.replace(" ", "-")
   add_res("Site.IO500.IOR." + op, "ior-" + t, "GiB/s")
 
@@ -31,9 +36,14 @@ for op in ["easy write", "easy stat", "easy delete", "hard write", "hard stat", 
   t = op.replace(" ", "-")
   add_res("Site.IO500.MDTest." + op, "mdtest-" + t, "kOP/s")
 
+add_res("Site.IO500.MDWorkbench.bench", "mdworkbench-bench", "kOP/s")
+
 add_res("Site.IO500.find.mixed", "find", "kOP/s")
+add_res("Site.IO500.find.easy", "find-easy", "kOP/s")
+add_res("Site.IO500.find.hard", "find-hard", "kOP/s")
 
 add_res("Site.IO500.score", "SCORE", "", "SCORE")
+add_res("Site.IO500.scoreX", "SCOREX", "", "SCORE")
 add_res("Site.IO500.scoreMD", "SCORE", "kOP/s", "MD")
 add_res("Site.IO500.scoreBW", "SCORE", "GiB/s", "BW")
 
